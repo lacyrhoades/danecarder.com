63fe67d0cb6e32e749dab51229bf81a46d9f70ac
Lacy Rhoades <lacyrhoades@gmail.com>
2011-01-08 16:50:47 -0600

    added global permissions to the upload dir

--- lib/form/doctrine/dcPaintingForm.class.php
+++ lib/form/doctrine/dcPaintingForm.class.php
@@ -12,5 +12,13 @@ class dcPaintingForm extends BasedcPaintingForm
 {
   public function configure()
   {
+    $image_path = sfConfig::get('sf_upload_dir');
+    $thumb_path = $image_path . '/thumbnails';
+    
+    $this->widgetSchema['image'] = new sfWidgetFormInputFile(array());
+    $this->validatorSchema['image'] = new sfValidatorFile(array('path'=>$image_path));
+    
+    $this->widgetSchema['thumbnail'] = new sfWidgetFormInputFile(array());
+    $this->validatorSchema['thumbnail'] = new sfValidatorFile(array('path'=>$thumb_path));
   }
 }
