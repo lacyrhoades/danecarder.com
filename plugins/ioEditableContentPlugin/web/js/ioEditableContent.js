04c3588e87473a0cf269f26153940df997c4851c
Lacy Rhoades <lacyrhoades@gmail.com>
2011-02-15 22:30:52 -0600

    fix for new version of metadata or something

--- plugins/ioEditableContentPlugin/web/js/ioEditableContent.js
+++ plugins/ioEditableContentPlugin/web/js/ioEditableContent.js
@@ -250,7 +250,10 @@ $.widget('ui.ioEditableContent', {
     params.model = this.option('model');
     params.pk = this.option('pk');
     params.fields = this.option('fields');
-    params.default_values = this.option('default_values');
+    if (this.option('default_values'))
+    {
+      params.default_values = this.option('default_values');
+    }
 
     if(typeof(this.option('form')) !== 'undefined')
     {
